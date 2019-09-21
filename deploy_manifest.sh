ee5cda99f08d36565674c08dbad8c4adee8aa873
ivan <ivanpilot@gmail.com>
2019-09-21 20:09:04 +0200

    corrected typos in deploy_manifest file

--- deploy_manifest.sh
+++ deploy_manifest.sh
@@ -20,8 +20,8 @@ if [ "${1:0:2}" = "--" ]; then
         statefulSetName="${2}"
         mongod_manifest_file="${3}.yml"
         storage_manifest_file="${4}.yml"
-        storage_name="${5}.yml"
-    
+        storageName="${5}"
+
     # Deploy storage class
         kubectl apply -f "${storage_manifest_file}"
         sleep 10
@@ -31,7 +31,6 @@ if [ "${1:0:2}" = "--" ]; then
         maxStorage=15
         while [[ "${name}" != "${storageName}" && "${counterStorage}" -le "${maxStorage}" ]]; do
             read name provisioner age <<< $(kubectl get storageClass ${storageName} | grep ${storageName})
-            sleep 2
             (( counterStorage++ ))
         done
 
@@ -40,32 +39,32 @@ if [ "${1:0:2}" = "--" ]; then
             exit 1
         fi
 
-    # # Deploy mongodb
-    #     kubectl apply -f "${mongod_manifest_file}"
-    #     sleep 20
+    # Deploy mongodb
+        kubectl apply -f "${mongod_manifest_file}"
+        sleep 20
 
-    #     # Check if all replicas started up
-    #     counter=0
-    #     max=15
-    #     while [[ "${numPodsRunning}" -ne "${replicas}" ]]; do
-    #         numPodsRunning=0
+        # Check if all replicas started up
+        counter=0
+        max=15
+        while [[ "${numPodsRunning}" -ne "${replicas}" ]]; do
+            numPodsRunning=0
             
-    #         if [ "${counter}" -eq "${max}" ]; then
-    #             echo "The replica set did not manage to start properly. Abort."
-    #             exit 1
-    #         fi
+            if [ "${counter}" -eq "${max}" ]; then
+                echo "The replica set did not manage to start properly. Abort."
+                exit 1
+            fi
 
-    #         for (( i = 0; i < "${replicas}"; i++ )); do
-    #             read pods[$i] ready[$i] status[$i] age[$i] <<< $(kubectl get pods ${pods[$i]} | grep ${statefulSetName})
+            for (( i = 0; i < "${replicas}"; i++ )); do
+                read pods[$i] ready[$i] status[$i] age[$i] <<< $(kubectl get pods ${pods[$i]} | grep ${statefulSetName})
 
-    #             if [ ${status[$i]} == 'Running' ]; then
-    #                 (( numPodsRunning++ ))
-    #             fi
-    #         done
+                if [ ${status[$i]} == 'Running' ]; then
+                    (( numPodsRunning++ ))
+                fi
+            done
 
-    #         sleep 5
-    #         (( counter++ ))
-    #     done
+            sleep 5
+            (( counter++ ))
+        done
     else
         echo "The number of replicas must be between 1 and 10."
         exit 1
