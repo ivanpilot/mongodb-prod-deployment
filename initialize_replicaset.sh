459e295bd6f771f2bf85541a062a7b6cccf66ddc
ivan <ivanpilot@gmail.com>
2019-09-21 22:25:33 +0200

    removed useless files and corrected typo in replicaset initializing file

--- initialize_replicaset.sh
+++ initialize_replicaset.sh
@@ -54,10 +54,10 @@ if [ "${1:0:2}" = "--" ]; then
         
         # Initialize the replica set inside the mongo container
         kubectl exec "${statefulSetObject}"-0 -c "${containerName}" -- bash -ec "mongo << EOF
-            rs.initiate(${initializeContent})
+            rs.initiate($(initializeContent))
 EOF"
 
-        # Wait for replica set to initialize
+        echo "Waiting for replica set to initialize."
         sleep 20
 
         echo "Checking if all replicas set were initialized." 
@@ -77,7 +77,7 @@ EOF" > tempRepSet.txt
             isReplicaSetCreated=$(tail -n 2 tempRepSet.txt | grep -v "^bye")
             rm ./tempRepSet.txt
             (( counter++ ))
-            sleep 5
+            sleep 2
         done
 
         if [ "${isReplicaSetCreated}" == "false" ]; then
