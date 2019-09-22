ca8e181c1de2c4e5a979dfef78b3df3204cba793
ivan <ivanpilot@gmail.com>
2019-09-22 23:40:25 +0200

    removed unused files

--- script1.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-echo "hello from script 1"
-echo "reset atc to 6"
-atc=6
-echo "atc value is ${atc}"
-echo "-------------------"
--- script2.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-echo "hello from script 2"
-echo "increase atc value by 1"
-(( atc + 1 ))
-echo "atc value is now ${atc}"
-echo "-------------------"
